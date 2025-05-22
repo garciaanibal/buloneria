050378c0c166543201bc1a7be3bb531da3a46466
anibal <garciaanibal>
2025-05-22 10:48:35 -0300

    aplicacion de estilos al login

--- assets/app.js
+++ assets/app.js
@@ -6,6 +6,8 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+// import './styles/signin.css';
+
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
 

9ec577baa872b837ec3d6df95b793dcb2c5f35d7
anibal <garciaanibal>
2025-05-23 08:46:48 -0300

    se agrega 2 decimales en precios en productos

---
+++ migrations/Version20250522153228.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250522153228 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE product CHANGE purchase_price purchase_price NUMERIC(10, 2) NOT NULL, CHANGE sale_price sale_price NUMERIC(10, 2) NOT NULL
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE product CHANGE purchase_price purchase_price NUMERIC(10, 0) NOT NULL, CHANGE sale_price sale_price NUMERIC(10, 0) NOT NULL
+        SQL);
+    }
+}
